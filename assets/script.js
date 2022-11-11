a0e1e9a0eed42d63e31710f0e07cd10eda3f8e18
McKenna Lawson <mckenna.lawson01@gmail.com>
2022-11-11 15:56:12 -0800

    start script

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+let timerEl = document.querySelector('.timer');
+let start = document.querySelector('#start');
+
+let $homeEl = $('#home');
+let $leaderboardEl = $('#leaderboard');
+let $lerderboardbodyEl = $('#leaderboard-body');
+let $questionEl = $('#question');
+let $buttonEl = $('.list-group'); 

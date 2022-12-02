afc31892178d960f64b7a8a5b5bfeca0ba2b731e
McKenna Lawson <mckenna.lawson01@gmail.com>
2022-12-01 19:08:40 -0600

    probably made a comment

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,45 @@ let $leaderboardEl = $('#leaderboard');
 let $lerderboardbodyEl = $('#leaderboard-body');
 let $questionEl = $('#question');
 let $buttonEl = $('.list-group'); 
+
+let question = 0;
+let randomized = []
+let score = 0;
+let currentQuestion = []
+let win = false;
+
+let questionBank = [
+  {
+    question: 'What does the === check for?',
+    answer: 'Strict Equality',
+    multipleChoice: [
+      'Loose Equality',
+      'Strict Equality',
+      'Total Equality',
+      'Random Equality'
+    ]
+  }
+]
+//got waaaaaaay ahead of myself here. references other code.
+
+// fisher-yates model spencer showed me! dope.
+// will not pick the same choice in an array more than once.
+
+// function shuffleArray(a) {
+//   for (let i = a.length -1; i > 0; i--) {
+//     let j = Math.floor(math.random() * (i + 1));
+//     let temp = array[i];
+//     array [i] = array[j];
+//     array[j] = temp;
+//   }
+//   return array;
+// }
+
+// function newQuestion() {
+//   currentQuestion = randomized[questionNum];
+//   let choice = shuffleArray(currentQuestion.multipleChoice);
+//   $butt
+// }
+
+// _____________START GAME_______________________________
+start.addEventListener(click, startGame);
